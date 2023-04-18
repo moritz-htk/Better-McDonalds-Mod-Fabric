2a0317c2dfbc152de60865e60a86eedb645ba2b8
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-04-18 20:48:33 +0200

    Initial commit

---
+++ src/main/java/simon_mc/bettermcdonaldsmod/BetterMcDonaldsModClient.java
@@ -0,0 +1,10 @@
+package simon_mc.bettermcdonaldsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterMcDonaldsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/simon_mc/bettermcdonaldsmod/DataGenerators.java
@@ -0,0 +1,11 @@
+package simon_mc.bettermcdonaldsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenerators implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
