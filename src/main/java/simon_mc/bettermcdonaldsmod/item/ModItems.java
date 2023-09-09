8439c27773382594ee72e6a1efb6453b4bfb8f4a
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-09-09 16:05:09 +0200

    Added lettuce

--- src/main/java/simon_mc/bettermcdonaldsmod/item/ModItems.java
+++ src/main/java/simon_mc/bettermcdonaldsmod/item/ModItems.java
@@ -14,6 +14,7 @@ import simon_mc.bettermcdonaldsmod.item.custom.KnifeItem;
 
 public class ModItems {
     public static final Item SALT = registerItem("salt", new Item(new FabricItemSettings()));
+    public static final Item LETTUCE = registerItem("lettuce", new Item(new FabricItemSettings().food(registerFoodValues(2, 1f).build())));
     public static final Item CHEESE = registerItem("cheese", new Item(new FabricItemSettings().food(registerFoodValues(3, 1.5f).meat().build())));
     public static final Item BEEF_PATTY = registerItem("beef_patty", new Item(new FabricItemSettings().food(registerFoodValues(4, 2f).meat().build())));
     public static final Item COOKED_BEEF_PATTY = registerItem("cooked_beef_patty", new Item(new FabricItemSettings().food(registerFoodValues(6, 3f).meat().build())));
