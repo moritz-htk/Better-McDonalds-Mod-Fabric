5ba424ec21a502790a7a3fdeab2e135be608d090
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-10-27 22:19:43 +0200

    Changed ModLootTableModifiers.java

--- src/main/java/simon_mc/bettermcdonaldsmod/loot/ModLootTableModifiers.java
+++ src/main/java/simon_mc/bettermcdonaldsmod/loot/ModLootTableModifiers.java
@@ -1,7 +1,9 @@
 package simon_mc.bettermcdonaldsmod.loot;
 
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.item.Item;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
@@ -14,21 +16,15 @@ public class ModLootTableModifiers {
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            if(GRASS_BLOCK_ID.equals(id)) {
-                LootPool.Builder tomatoPoolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.15f)).with(ItemEntry.builder(ModItems.TOMATO_SEEDS));
-                tableBuilder.pool(tomatoPoolBuilder.build());
-
-                LootPool.Builder lettucePoolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.15f)).with(ItemEntry.builder(ModItems.LETTUCE_SEEDS));
-                tableBuilder.pool(lettucePoolBuilder.build());
-            }
-
-            if(FERN_BLOCK_ID.equals(id)) {
-                LootPool.Builder tomatoPoolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.15f)).with(ItemEntry.builder(ModItems.TOMATO_SEEDS));
-                tableBuilder.pool(tomatoPoolBuilder.build());
-
-                LootPool.Builder lettucePoolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.15f)).with(ItemEntry.builder(ModItems.LETTUCE_SEEDS));
-                tableBuilder.pool(lettucePoolBuilder.build());
+            if (GRASS_BLOCK_ID.equals(id) || FERN_BLOCK_ID.equals(id)) {
+                addSeedLoot(tableBuilder, ModItems.TOMATO_SEEDS);
+                addSeedLoot(tableBuilder, ModItems.LETTUCE_SEEDS);
             }
         });
+    }
+
+    private static void addSeedLoot(LootTable.Builder tableBuilder, Item item) {
+        LootPool.Builder poolBuilder = LootPool.builder().rolls(ConstantLootNumberProvider.create(1)).conditionally(RandomChanceLootCondition.builder(0.15f)).with(ItemEntry.builder(item));
+        tableBuilder.pool(poolBuilder.build());
     }
 }
