5d77d1c7db8590446733005ac397a1cdacfca52e
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-10-27 23:04:29 +0200

    Changed ModItemGroup.java

--- src/main/java/simon_mc/bettermcdonaldsmod/item/ModItemGroup.java
+++ src/main/java/simon_mc/bettermcdonaldsmod/item/ModItemGroup.java
@@ -11,45 +11,44 @@ import simon_mc.bettermcdonaldsmod.BetterMcDonaldsMod;
 import simon_mc.bettermcdonaldsmod.block.ModBlocks;
 
 public class ModItemGroup {
-    public static ItemGroup BETTER_MCDONALDS_MOD_TAB = Registry.register(Registries.ITEM_GROUP, new Identifier(BetterMcDonaldsMod.MOD_ID, "bettermcdonaldsmod"),
-            FabricItemGroup.builder()
-                    .displayName(Text.literal("Better McDonald's Mod"))
-                    .icon(() -> new ItemStack(ModItems.HAPPY_MEAL))
-                    .entries((displayContext, entries) -> {
-                        entries.add(ModBlocks.SALT_BLOCK);
-                        entries.add(ModItems.SALT);
-                        entries.add(ModItems.TOMATO);
-                        entries.add(ModItems.TOMATO_SEEDS);
-                        entries.add(ModItems.LETTUCE);
-                        entries.add(ModItems.LETTUCE_SEEDS);
-                        entries.add(ModItems.CHEESE);
-                        entries.add(ModItems.TORTILLA);
-                        entries.add(ModItems.BEEF_PATTY);
-                        entries.add(ModItems.COOKED_BEEF_PATTY);
-                        entries.add(ModItems.RAW_BACON);
-                        entries.add(ModItems.COOKED_BACON);
-                        entries.add(ModItems.MAYONNAISE);
-                        entries.add(ModItems.SWEET_SOUR_SAUCE);
-                        entries.add(ModItems.KETCHUP);
-                        entries.add(ModItems.MUSTARD);
-                        entries.add(ModItems.HAMBURGER);
-                        entries.add(ModItems.CHEESEBURGER);
-                        entries.add(ModItems.MCBACON);
-                        entries.add(ModItems.BIG_MAC);
-                        entries.add(ModItems.CHICKENBURGER);
-                        entries.add(ModItems.MCCHICKEN);
-                        entries.add(ModItems.FILET_O_FISH);
-                        entries.add(ModItems.MCWRAP);
-                        entries.add(ModItems.CHICKEN_MCNUGGETS);
-                        entries.add(ModItems.FRIES);
-                        entries.add(ModItems.HAPPY_MEAL);
-                        entries.add(ModItems.COCA_COLA);
-                        entries.add(ModItems.FANTA);
-                        entries.add(ModItems.SPRITE);
-                        entries.add(ModItems.LIPTON_ICE_TEA_PEACH);
-                        entries.add(ModItems.MCFLURRY);
-                        entries.add(ModItems.KNIFE);
-                    }).build());
+    public static ItemGroup BETTER_MCDONALDS_MOD_TAB = Registry.register(Registries.ITEM_GROUP, new Identifier(BetterMcDonaldsMod.MOD_ID, BetterMcDonaldsMod.MOD_ID), FabricItemGroup.builder()
+            .displayName(Text.literal("Better McDonald's Mod"))
+            .icon(() -> new ItemStack(ModItems.HAPPY_MEAL))
+            .entries((displayContext, entries) -> {
+                entries.add(ModBlocks.SALT_BLOCK);
+                entries.add(ModItems.SALT);
+                entries.add(ModItems.TOMATO);
+                entries.add(ModItems.TOMATO_SEEDS);
+                entries.add(ModItems.LETTUCE);
+                entries.add(ModItems.LETTUCE_SEEDS);
+                entries.add(ModItems.CHEESE);
+                entries.add(ModItems.TORTILLA);
+                entries.add(ModItems.BEEF_PATTY);
+                entries.add(ModItems.COOKED_BEEF_PATTY);
+                entries.add(ModItems.RAW_BACON);
+                entries.add(ModItems.COOKED_BACON);
+                entries.add(ModItems.MAYONNAISE);
+                entries.add(ModItems.SWEET_SOUR_SAUCE);
+                entries.add(ModItems.KETCHUP);
+                entries.add(ModItems.MUSTARD);
+                entries.add(ModItems.HAMBURGER);
+                entries.add(ModItems.CHEESEBURGER);
+                entries.add(ModItems.MCBACON);
+                entries.add(ModItems.BIG_MAC);
+                entries.add(ModItems.CHICKENBURGER);
+                entries.add(ModItems.MCCHICKEN);
+                entries.add(ModItems.FILET_O_FISH);
+                entries.add(ModItems.MCWRAP);
+                entries.add(ModItems.CHICKEN_MCNUGGETS);
+                entries.add(ModItems.FRIES);
+                entries.add(ModItems.HAPPY_MEAL);
+                entries.add(ModItems.COCA_COLA);
+                entries.add(ModItems.FANTA);
+                entries.add(ModItems.SPRITE);
+                entries.add(ModItems.LIPTON_ICE_TEA_PEACH);
+                entries.add(ModItems.MCFLURRY);
+                entries.add(ModItems.KNIFE);
+            }).build());
 
     public static void registerItemGroups() {
         BetterMcDonaldsMod.LOGGER.info("Registering ModItemGroups for " + BetterMcDonaldsMod.MOD_ID);
